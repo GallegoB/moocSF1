5d388e3db44f84b1c04c1a8150c76b0cceb4d91e
Gallego Baptiste <bapgallego@gmail.com>
2023-07-10 15:57:33 +0200

    Chapitre 21

--- src/Controller/JeuController.php
+++ src/Controller/JeuController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\JeuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JeuController extends AbstractController
 {
-    #[Route('/jeu', name: 'app_jeu_test')]
-    public function index(): Response
+    #[Route('/jeu', name: 'app_jeu')]
+    public function index(JeuRepository $jeuRepository): Response
     {
+        $jeux = $jeuRepository->findJeuxWithLimitedNumber(3);
+
+
         return $this->render('jeu/index.html.twig', [
-            'controller_name' => 'JeuController',
+            'jeux' => $jeux,
         ]);
     }
 }
